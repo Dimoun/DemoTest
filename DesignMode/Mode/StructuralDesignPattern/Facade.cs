6c6eff5440962cfe1f3be5f1d6fed41160f67f06
Jaxson <jaxson.shi@ethealthcare.com.cn>
2024-10-09 14:48:58 +0800

    Add SoftwareDeveloper/Note and FacadeMode

---
+++ DesignMode/Mode/StructuralDesignPattern/Facade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode.Mode.StructuralDesignPattern
+{
+    internal class Facade
+    {
+        private SubSystemA subSystemA = new SubSystemA();
+        private SubSystemB subSystemB = new SubSystemB();
+        
+        public void Operation()
+        {
+            subSystemA.Operation();
+            subSystemB.Operation();
+        }
+    }
+    
+    public class SubSystemA 
+    {
+        public void Operation()
+        {
+
+        }
+    }
+
+    public class SubSystemB 
+    {
+        public void Operation()
+        {
+
+        }
+    }
+
+    public class FacadeMode
+    {
+       public void Create() 
+       {
+            Facade facade = new Facade();
+            facade.Operation();
+        }
+    }
+}
